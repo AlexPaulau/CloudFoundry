ebe51b51a371a8f70c2efd34e64119721d21ad5a
Aliaksandr Paulau <aliaksandr.paulau@altoros.com>
2016-12-12 19:15:41 +0300

    code for application

---
+++ src/HelloWorld/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace HelloWorld
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context =>
+            {
+                return context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
